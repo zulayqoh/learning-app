8c9763a56449784716a41581af9e3183923d6306
t3mye <babatundetemi1@yahoo.com>
2023-10-11 13:56:53 +0100

    read state on second page

--- lib/main.dart
+++ lib/main.dart
@@ -29,34 +29,87 @@ class MyHomePage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    // use ref.watch to get/read/access the preserved state
     int homepageCount = ref.watch(appCount);
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: const Text("Flutter App"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'You have pushed the button this many times:',
+              ),
+              Text(
+                // preserved state can be read - ${ref.read(appCount)} or accessed
+                // the notifier notifies the ui of the changes on the state
+                '$homepageCount',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            FloatingActionButton(
+              onPressed: () {
+                // use ref.read to make modification on preserved state
+                ref.read(appCount.notifier).state++;
+              },
+              heroTag: 'increment button',
+              tooltip: 'Increment',
+              child: const Icon(Icons.add),
+            ),
+            FloatingActionButton(
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (BuildContext context) => const SecondPage()));
+              },
+              heroTag: 'next button',
+              tooltip: 'Next',
+              child: const Icon(Icons.arrow_forward_ios),
+            ),
+          ],
+        ));
+  }
+}
+
+class SecondPage extends ConsumerWidget {
+  const SecondPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    int secondpageCount = ref.watch(appCount);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text("Flutter App"),
+        title: const Text('Second Page'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              // preserved state can be read - ${ref.read(appCount)} or accessed
-              '$homepageCount',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+          children: [
+            const Text("Shows Preserved state from the previous page"),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "preserved data is ",
+                  style: Theme.of(context).textTheme.headlineSmall,
+                ),
+                Text(
+                  '$secondpageCount',
+                  style: Theme.of(context).textTheme.displayMedium,
+                )
+              ],
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          ref.read(appCount.notifier).state++;
-        },
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
